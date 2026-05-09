45570f1a3c153f6efd0fbb2dd151530fb83856b4
Evlumba Dev <dev@evlumba.com>
2026-05-10 02:44:09 +0300

    Enable Android edge-to-edge

--- android/app/src/main/kotlin/com/evlumba/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/evlumba/app/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.evlumba.app
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+        }
+    }
+}
